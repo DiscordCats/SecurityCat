5b5d5e33543371784360b1b76ecbb332623b3cdd
Jay <me@jayxtq.xyz>
2024-08-03 10:49:38 +0100

    feat: new schema

--- src/schema.ts
+++ src/schema.ts
@@ -1,17 +1,15 @@
-import {pgTable, serial, varchar, text, json, PgArray, integer} from 'drizzle-orm/pg-core'
+import {pgTable, text, jsonb, integer} from 'drizzle-orm/pg-core'
+
+export type Modules = {
+    name: string;
+    id: string;
+    log: string;
+    duration: string;
+    bypass: string[];
+}
+
 
 export const servers = pgTable('servers', {
     id: text('id').primaryKey(),
-    modules: text('modules').array(),
-    automod_ids: text('automod_ids').array(),
-    automod_channel: text('automod_channel').notNull(),
-    extras: integer('extras').references(() => extraRules.id)
+    modules: jsonb('modules').$type<Modules[]>().default([]).notNull()
 });
-
-
-export const extraRules = pgTable('extra_rules', {
-    id: serial('id').primaryKey(),
-    words: text('words').array(),
-    regex: text('regex').array(),
-    allowed: text('allowed').array(),
-})
