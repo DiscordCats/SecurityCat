1ef83707a65ddeeaa47d47c8bb9081b817b16367
Jay <me@jayxtq.xyz>
2024-08-04 18:37:48 -0400

    feat: modules route
    Signed-off-by: rad <me@radnotred.dev>

---
+++ src/routes/modules.ts
@@ -0,0 +1,18 @@
+import { Client } from 'discord.js';
+import { Hono } from 'hono';
+import { authenticate } from '../utils/discord';
+import { db } from '../db';
+import { servers } from '../schema';
+import { eq } from 'drizzle-orm';
+
+export default function (app: Hono, client: Client) {
+    app.get('/modules', async (c) => {
+        const auth = await authenticate(c);
+        if (!auth) return c.json({ error: 'Unauthorized' }, 401);
+        const modules = await db
+            .select({ modules: servers.modules })
+            .from(servers)
+            .where(eq(servers.id, auth));
+        return c.json(modules[0]?.modules || []);
+    });
+}
--- src/utils/discord.ts
+++ src/utils/discord.ts
@@ -1,8 +1,31 @@
-import { Client } from 'discord.js';
+import { Client, GuildMember, PermissionFlagsBits } from 'discord.js';
+import axios from 'axios';
+import { Context } from 'hono';
 
 export async function fetchGuild(client: Client, guildId: string) {
     return (
         client.guilds.cache.get(guildId) ||
         (await client.guilds.fetch(guildId).catch(() => null))
     );
+}
+
+export async function authenticate(c: Context) {
+    const serverId = new URL(c.req.url).searchParams.get('serverId');
+    const token = c.req.header('Authorization');
+    if (!token || !serverId) return false;
+    const member = await axios
+        .get<GuildMember>(
+            `https://discord.com/api/v10/users/@me/guilds/${serverId}/member`,
+            {
+                headers: {
+                    Authorization: token,
+                },
+            },
+        )
+        .catch(() => null);
+    if (!member || member.status !== 200) return false;
+    if (member.data.user.bot) return false;
+    if (!(member.data.permissions.bitfield & PermissionFlagsBits.ManageGuild))
+        return false;
+    return serverId;
 }
