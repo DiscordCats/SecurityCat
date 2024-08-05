f7430259db0ef66fbf1f5f064a2a0780aae176ea
rad <me@radnotred.dev>
2024-08-05 15:39:08 -0400

    feat: message edit event

--- src/events/discord/messageEdit.ts
+++ src/events/discord/messageEdit.ts
@@ -5,19 +5,20 @@ import { sendLog } from '../../utils/discord';
 import rules from '../../utils/rulesets';
 
 export default async function (client: Client) {
-    client.on('messageCreate', async (message) => {
-        if (!message.guild) return;
-        const modules = (await getServerModules(message.guild.id)).filter(module => module.enabled);
+    client.on('messageUpdate', async (oldMessage, newMessage) => {
+        if (!newMessage.guild) return;
+        if (!newMessage.content || newMessage.content === oldMessage.content) return;
+        const modules = (await getServerModules(newMessage.guild.id)).filter(module => module.enabled);
         if (modules.length === 0) return;
 
-        const memberRoles = message.member?.roles.cache.map(role => role.id) || [];
+        const memberRoles = newMessage.member?.roles.cache.map(role => role.id) || [];
 
-        let content = message.content.toLowerCase();
+        let content = newMessage.content.toLowerCase();
 
         for (const module of modules) {
             const ruleSet = rules[module.name];
             if (!ruleSet) continue;
-            if (module.bypass.channels.includes(message.channel.id)) continue;
+            if (module.bypass.channels.includes(newMessage.channel.id)) continue;
             if (memberRoles.some(roleId => module.bypass.roles.includes(roleId))) continue;
 
             for (const word of module.bypass.words) {
@@ -26,7 +27,7 @@ export default async function (client: Client) {
             }
 
             if (ruleSet.words.some(word => content.includes(word.toLowerCase()))) {
-                await punish(client, message as Message<true>, module);
+                await punish(client, newMessage as Message<true>, module);
                 break;
             }
         }
@@ -36,6 +37,7 @@ export default async function (client: Client) {
 async function punish(client: Client, message: Message<true>, module: Modules) {
     try {
         await sendLog(client, message.guild.id, module.log, '');
+
         if (module.blockMessageEnabled) await message.delete();
 
         if (module.duration) {
